bdf5dd2ef26510eeea75db51060d51785b9fb59f
aalissonaq <aalissonalmeidaq@gmail.com>
2021-12-14 20:10:04 -0300

    Função de geração da UUID v4 no arquivo util

--- util/util.php
+++ util/util.php
@@ -1,6 +1,6 @@
 <?php
 
-function sweetalert($title, $message, $type, $time, $position=null)
+function sweetalert($title, $message, $type, $time, $position = null)
 {
   $position = $position ? $position : 'center';
   echo "<script type='text/javascript'>
@@ -78,4 +78,19 @@ function GetDirectoryPercentageUsed($path, $storage, $unit = null)
 
   $percentage = ($bytestotal  * 100) / $storage;
   return number_format($percentage, 2, '.', '');
+}
+
+function uuidv4($data = null)
+{
+  // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+  $data = $data ?? random_bytes(16);
+  assert(strlen($data) == 16);
+
+  // Set version to 0100
+  $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+  // Set bits 6-7 to 10
+  $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+  // Output the 36 character UUID.
+  return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
